12499dfa704370da2530ab1e3fd60746383c7433
Mark <mark@MarkUbuntuServer.0.2.15>
2016-10-17 19:07:02 +0200

    Working, but awkward version

---
+++ client/client.py
@@ -0,0 +1,14 @@
+import cv2
+import requests
+import base64
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    print("==================================================")
+    ret,frame = cap.read()
+    cv2.imwrite("temp.jpg",frame)
+    temp=open("temp.jpg","rb")
+    binary=temp.read()
+    response=requests.post("http://127.0.0.1:5000/upload", data=base64.b64encode(binary))
+    print(response.status_code)
---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import *
+import os
+import cv2
+import base64
+import numpy
+
+app = Flask(__name__, template_folder ='templates')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route("/upload", methods=['POST'])
+def upload():
+    print("A POST request has come!")
+
+    temp=bytearray(base64.b64decode(request.data))
+    file=open("static/temp.jpg",'wb')
+    file.write(temp)
+
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
+
